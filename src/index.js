a2a60a016a62ae570090d41933e4055d1e04c9eb
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-04 13:19:32 -0500

    Creating a server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Works!')
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.listen(port)
+console.log('Server running in port: ',port)
