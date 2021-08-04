458cbba4192eb2f17b40ff17f84525300340917f
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-04 14:22:21 -0500

    Adding info our app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import morgan from 'morgan'
+import pkg from '../package.json'
 
 const app = express()
+
+app.set('pkg',pkg)
 
 app.use(express.json())
 app.use(express.urlencoded({
@@ -11,6 +14,11 @@ app.use(morgan("dev"))
 
 app.get('/', (req,res)=>{
     return res.json({
+        name: app.get('pkg').name,
+        description: app.get('pkg').description,
+        author: app.get('pkg').author,
+        version: app.get('pkg').version,
+        github: app.get('pkg').repository.url,
         msg: 'WELCOME TO MY REST API'
     })
 })
