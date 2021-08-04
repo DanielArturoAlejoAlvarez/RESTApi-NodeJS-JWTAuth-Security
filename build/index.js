7b58f9b73e35a8e58f10738dd1f2ff45e51a98b3
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-04 13:31:19 -0500

    Configure to production our app

---
+++ build/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var app = (0, _express["default"])();
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Server running in port: ', port);
