9bc4a3b1f791af92df918262ffccfadbca42fc23
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-05 10:11:44 -0500

    Creating Product model

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,3 +1,5 @@
+import Product from '../models/Product'
+
 export const getProducts = async (req,res)=>{
 
     
---
+++ src/models/Product.js
@@ -0,0 +1,27 @@
+import { Schema,model } from 'mongoose'
+
+const productSchema = new Schema({
+    name: String,
+    price: Number,
+    stock: Number,
+    category: {
+        type: String,
+        enum: ['COMPUTER','TABLET','SMARTPHONE','ACCESSORIES'],
+        default: 'COMPUTER'
+    },
+    imgURL: {
+        type: String,
+        maxLength: 512
+    },
+    status: {
+        type: Boolean,
+        default: true
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false
+    }
+)
+
+export default model('Product',productSchema)
