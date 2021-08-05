8d03ed3e008466952ca8d03c4a842f9fd0943010
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-05 13:40:44 -0500

    Creating register method in Auth Controller

--- src/controllers/auth/auth.controllers.js
+++ src/controllers/auth/auth.controllers.js
@@ -1,8 +1,32 @@
-export const login = async (req,res)=>{
+import config from "../../config/config";
+import jwt from "jsonwebtoken";
+import User from "../../models/User";
 
-}
+export const login = async (req, res) => {};
 
+export const register = async (req, res) => {
+  const { displayName, username, email, password, avatar, roles, status } =
+    req.body;
 
-export const register = async (req,res)=>{
+  const newUser = new User({
+    displayName,
+    username,
+    email,
+    password: User.encryptPassword(password),
+    avatar,
+    roles,
+    status,
+  });
 
-}
+  const user = await newUser.save();
+
+  const token = jwt.sign({ id: user._id }, config.secret_key, {
+    expiresIn: 60 * 60,
+  });
+
+  return res.status(201).json({
+    msg: "User is registered!",
+    token,
+    user,
+  });
+};
