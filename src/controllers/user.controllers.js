6da4ec913d4fd4fa5a8b3c459cb14a25cde11642
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-05 15:40:30 -0500

    Creating getUsers method in User Controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,12 @@
-export const getUsers = async (req,res)=>{
+import User from "../models/User"
 
+export const getUsers = async (req,res)=>{
+    const users = await User.find().populate("roles")
+    return res.json(users)
 }
 
 export const getUser = async (req,res)=>{
-
+    
 }
 
 export const saveUser = async (req,res)=>{
