a3fa66ff00e23cfa8a715eb59ea4bd9402a692e5
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-05 11:59:27 -0500

    Creating Role model

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,42 +1,49 @@
-import Product from '../models/Product'
+import Product from "../models/Product";
 
-export const getProducts = async (req,res)=>{
-    const products = await Product.find()
-    return res.json(products)
-}
+export const getProducts = async (req, res) => {
+  const products = await Product.find();
+  return res.json(products);
+};
 
-export const getProduct = async (req,res)=>{
-    const { idProduct } = req.params
-    const product = await Product.findById(idProduct)
-    return res.json(product)
-}
+export const getProduct = async (req, res) => {
+  const { idProduct } = req.params;
+  const product = await Product.findById(idProduct);
+  return res.json(product);
+};
 
-export const saveProduct = async (req,res)=>{
-    const {name,price,stock,category,imgURL,status} = req.body
-    const newProduct = new Product({name,price,stock,category,imgURL,status})
-    const product = await newProduct.save()
+export const saveProduct = async (req, res) => {
+  const { name, price, stock, category, imgURL, status } = req.body;
+  const newProduct = new Product({
+    name,
+    price,
+    stock,
+    category,
+    imgURL,
+    status,
+  });
+  const product = await newProduct.save();
 
-    return res.status(201).json(
-        {
-            msg: 'Product saved successfully!',
-            product
-        }
-    )
-}
+  return res.status(201).json({
+    msg: "Product saved successfully!",
+    product,
+  });
+};
 
-export const updateProduct = async (req,res)=>{
-    const { idProduct } = req.params
-    const updProduct = await Product.findByIdAndUpdate(idProduct, req.body, {new: true})
-    return res.status(200).json({
-        msg: 'Product updated successfully!',
-        product: updProduct
-    })
-}
+export const updateProduct = async (req, res) => {
+  const { idProduct } = req.params;
+  const updProduct = await Product.findByIdAndUpdate(idProduct, req.body, {
+    new: true,
+  });
+  return res.status(200).json({
+    msg: "Product updated successfully!",
+    product: updProduct,
+  });
+};
 
-export const deleteProduct = async (req,res)=>{
-    const { idProduct } = req.params
-    await Product.findByIdAndDelete(idProduct)
-    return res.status(201).json({
-        msg: 'Product deleted successfully!'
-    })
-}
+export const deleteProduct = async (req, res) => {
+  const { idProduct } = req.params;
+  await Product.findByIdAndDelete(idProduct);
+  return res.status(201).json({
+    msg: "Product deleted successfully!",
+  });
+};
---
+++ src/models/Role.js
@@ -0,0 +1,9 @@
+import { Schema,model } from 'mongoose'
+
+const roleSchema = new Schema({
+    name: String
+}, {
+    versionKey: false
+})
+
+export default model('Role', roleSchema)
