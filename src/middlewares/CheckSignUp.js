78df39b0d4ec78131c7f8a7949c1524fa2be1c3e
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-07 12:44:28 -0500

    Creating checkEmailAndUsernameExists method in CheckSignUp middleware

--- src/middlewares/CheckSignUp.js
+++ src/middlewares/CheckSignUp.js
@@ -1,17 +1,34 @@
-import { ROLES } from '../models/Role'
+import { ROLES } from "../models/Role";
+import User from "../models/User";
 
-export const checkRolesExists = (req,res,next)=>{
-    const arrayRoles = req.body.roles
+export const checkEmailAndUsernameExists = async (req, res, next) => {
+  const username = await User.findOne({ username: req.body.username });
 
-    if (req.body.roles) {
-        for (let i = 0; i < arrayRoles.length; i++) {
-            if (!ROLES.includes(arrayRoles[i])) {
-                return res.status(401).json({
-                    msg: `Role ${arrayRoles[i]} does not exixts!`
-                })
-            }            
-        }
+  if (username) {
+    return res.status(400).json({ msg: "Username already exist!" });
+  }
+
+  const email = await User.findOne({ email: req.body.email });
+
+  if (email) {
+    return res.status(400).json({ msg: "Email already exist!" });
+  }
+
+  next();
+};
+
+export const checkRolesExists = (req, res, next) => {
+  const arrayRoles = req.body.roles;
+
+  if (req.body.roles) {
+    for (let i = 0; i < arrayRoles.length; i++) {
+      if (!ROLES.includes(arrayRoles[i])) {
+        return res.status(401).json({
+          msg: `Role ${arrayRoles[i]} does not exixts!`,
+        });
+      }
     }
+  }
 
-    next()
-}
+  next();
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,8 +3,9 @@ import {Router} from 'express'
 const router = Router() 
 
 import { login, register } from '../controllers/auth/auth.controllers'
+import { checkEmailAndUsernameExists,checkRolesExists } from '../middlewares/CheckSignUp'
 
 router.post('/login', login)
-router.post('/register', register)
+router.post('/register', [checkRolesExists,checkEmailAndUsernameExists], register)
 
 export default router
